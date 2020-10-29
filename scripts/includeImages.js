2358dc9009e9bb2fff46f5c32ad8f5cbdbe71c94
Zealuss <35547430+ZealussGaming@users.noreply.github.com>
2020-10-29 17:46:34 +1300

    Nicer layout and styling

--- scripts/includeImages.js
+++ scripts/includeImages.js
@@ -4,20 +4,23 @@ const addSections = function(parent, directory) {
     let sectEl = document.createElement('section');
     sectEl.classList.add("gallery-section");
 
-    let h = document.createElement("h1");
-    header = directory.substring(0, directory.length - 1);;
+    let hr = document.createElement('hr');
+
+    let h = document.createElement("h2");
+    let header = capitalizeFirstLetter(chop(directory))
     h.innerText = header;
 
     let div = document.createElement("div");
     div.classList.add("gallery-inner");
 
+    sectEl.append(hr);
     sectEl.append(h);
     sectEl.append(div);
     parent.append(sectEl);
     return div
 }
 
-
+//returns an array of directories
 const getDirectories = function(dpath) {
     return new Promise((resolve, reject) => {
         xhttp = new XMLHttpRequest();
@@ -47,22 +50,28 @@ const getDirectories = function(dpath) {
 
 //appends the image to the page
 const addImg = function(parent, src) {
-    let div = document.createElement("div");
-    div.classList.add("image-wrapper");
+    let btn = document.createElement("button");
+    btn.classList.add("image-wrapper");
 
-    
+
     let imgEl = document.createElement('img');
+    imgEl.src = src;
     
     let fileName = decodeURI(src.split('\\').pop().split('/').pop()).replace("_", " ");
     console.log(fileName);
     let p = document.createElement("p");
     p.innerText = fileName;
 
-    imgEl.src = src;
 
-    div.append(p);
-    div.append(imgEl);
-    parent.append(div);
+    btn.append(imgEl);
+    btn.append(p);
+    parent.append(btn);
+
+    
+    let width = imgEl.offsetWidth;
+    console.log(width);
+    btn.style.width = width+"px";
+    btn.addEventListener("click", showModal);
 }
 
 //get img sources
@@ -109,10 +118,31 @@ const loadImage = function(url) {
 }
 
 
+var loadSection = function() {
+
+}
+
 //loads a gallery
-const loadGallery = function(parentSelector, gallery) {
+var loadGallery = function(parentSelector, gallery) {
+    //control
+    var selectEl = document.querySelector("#controls select");
+
+    getDirectories("images/")
+    .then((directories) => {
+        directories.forEach((directory) => {
+            var option = document.createElement("option");
+            directoryName = chop(directory);
+            option.value = directoryName;
+            option.innerText = capitalizeFirstLetter(directoryName);
+            selectEl.append(option);
+            selectEl.value = gallery;
+        });
+    });
+
+
+    //gallery sections
     var galleryPath = "images/"+encodeURIComponent(gallery)+"/";
-    var parentEl = document.querySelector(parentSelector);
+    var galleryEl = document.querySelector(parentSelector);
     getDirectories(galleryPath)
          
     .then((directories) =>{
@@ -121,7 +151,7 @@ const loadGallery = function(parentSelector, gallery) {
             var directoryPath = galleryPath + directory;
             
             console.log(directoryPath);
-            sectionEl = addSections(parentEl, directory);
+            sectionEl = addSections(galleryEl, directory);
             getimages(directoryPath).then((files) => {
                 console.log(files);
                 files.forEach((file) => {
@@ -137,3 +167,18 @@ const loadGallery = function(parentSelector, gallery) {
     });
 } 
     
+//Changes the gallery
+var handleGalleryChange = function() {
+    console.log(event)
+    gallery = event.target.value;
+    document.location.search = "?gallery="+gallery;
+    // loadGallery("article.gallery", gallery);
+} 
+
+
+const galleryCB = function() {
+    console.log("Gallery script ran")
+    let searchParams = new URLSearchParams(window.location.search);
+    let gallery = searchParams.get("gallery");
+    loadGallery("article.gallery", gallery);
+}
